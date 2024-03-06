5f03a2b445b902b3cb8669e544907d3ba8fa7e61
quinnmike82 <87664191+quinnmike82@users.noreply.github.com>
2024-03-06 21:39:28 +0700

    [QLN-98] Global search using search full text (#20)

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,25 @@
+﻿using fuquizlearn_api.Entities;
+using fuquizlearn_api.Models.Request;
+using fuquizlearn_api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fuquizlearn_api.Controllers
+{
+    [Route("api/SearchGlobal")]
+    [ApiController]
+    public class SearchController : BaseController
+    {
+        private ISearchTextService _searchTextService;
+        public SearchController(ISearchTextService searchService)
+        {
+            _searchTextService = searchService;
+        }
+        [HttpGet("SearchGlobal")]
+        public async Task<IActionResult> GetAllPosts([FromQuery] PagedRequest options)
+        {
+            var result = await _searchTextService.Search(options);
+            return Ok(result);
+        }
+    }
+}
