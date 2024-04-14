63dcc27a09decc247585fd2c382c3aa7fc79037f
SaNgoTin <phuoctungpbc02@gmail.com>
2024-04-14 17:23:17 +0700

    [QLN-000] change to asynchonous (#72)

--- Services/EmbeddingQueueService.cs
+++ Services/EmbeddingQueueService.cs
@@ -44,7 +44,7 @@ public class EmbeddingQueueService : IEmbeddingQueueService
             var _geminiAIService = scope.ServiceProvider.GetRequiredService<IGeminiAIService>();
 
             var quizbank = await _quizBankService.GetById(quizBankId);
-            var quizzes = _quizService.GetAll(quizBankId);
+            var quizzes = await _quizService.GetAll(quizBankId);
             var quizbankEmbeddingReponse = await EmbeddingQuizbank(quizbank, quizzes, _geminiAIService);
 
             if (quizbankEmbeddingReponse != null)
